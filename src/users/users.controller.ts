5021ffede0e70bb67540e9987c758cad6d532c2a
Matiullah Karimi <mkarimi@netlinks.af>
2023-03-29 18:05:38 +0400

    Add users crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Post('/')
+    signup(@Body() body: CreateUserDto) {
+       return this.usersService.signup(body.name, body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(+id);
+    }
+
+    @Get('/')
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(+id, body);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(+id);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+    signup(name: string, email: string, password: string) {
+        const user = this.userRepo.create({name, email, password});
+        this.userRepo.save(user);
+        return user;
+    }
+
+    findOne(id: number) {
+        const user = this.userRepo.findOne({where: { id }});
+
+        return user;
+    }
+
+    find(email: string) {
+        return this.userRepo.find({where: {email }});
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+
+        if (!user) {
+            throw new Error('user not found');
+        }
+
+        Object.assign(user, attrs);
+        this.userRepo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+
+        if (!user) {
+            throw new Error('user not found');
+        }
+
+        this.userRepo.remove(user);
+    }
+}
