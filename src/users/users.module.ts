f9a2271311103a9e5d37adbf1576d6036ecc1450
Matiullah Karimi <mkarimi@netlinks.af>
2023-04-02 08:08:26 +0400

    Hash users password while storing

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BcryptService } from 'src/core/services/bcrytp.service';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -7,6 +8,7 @@ import { UsersService } from './users.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, BcryptService],
+  exports: [UsersService]
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BcryptService } from 'src/core/services/bcrytp.service';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+    constructor(@InjectRepository(User) private userRepo: Repository<User>, private bcryptService: BcryptService) {}
 
-    signup(name: string, email: string, password: string) {
-        const user = this.userRepo.create({name, email, password});
+    async signup(name: string, email: string, password: string) {
+        const hashedPassword = await this.bcryptService.hash(password);
+
+        const user = this.userRepo.create({name, email, password: hashedPassword});
         this.userRepo.save(user);
         return user;
     }
